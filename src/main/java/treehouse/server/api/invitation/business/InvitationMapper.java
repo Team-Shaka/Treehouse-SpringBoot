4f9313d69730e52c261b15eb632214de5d27aea5
HyoBN <qls0786@naver.com>
2024-05-02 17:48:05 +0900

    :sparkles: Feat: Invitation 관련 로직 개발"

---
+++ src/main/java/treehouse/server/api/invitation/business/InvitationMapper.java
@@ -0,0 +1,34 @@
+package treehouse.server.api.invitation.business;
+
+import lombok.AccessLevel;
+import lombok.NoArgsConstructor;
+import org.springframework.stereotype.Component;
+import treehouse.server.api.invitation.presentation.dto.InvitationResponseDTO;
+import treehouse.server.global.entity.Invitation.Invitation;
+import treehouse.server.global.entity.User.User;
+import treehouse.server.global.entity.User.UserRole;
+import treehouse.server.global.entity.User.UserStatus;
+
+import java.util.List;
+
+@Component
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
+public class InvitationMapper {
+
+    public InvitationResponseDTO.getInvitation toGetInvitation (Invitation invitation, List<String> treeMemberProfileImages) {
+        return InvitationResponseDTO.getInvitation.builder()
+                .invitationId(invitation.getId())
+                .treehouseName(invitation.getTreeHouse().getName())
+                .senderName(invitation.getSender().getName())
+                .senderProfileImageUrl(invitation.getSender().getProfileImageUrl())
+                .treehouseSize(invitation.getTreeHouse().getMemberList().size())
+                .treehouseMemberProfileImages(treeMemberProfileImages)
+                .build();
+    }
+    public InvitationResponseDTO.getInvitations toGetInvitations(List<InvitationResponseDTO.getInvitation> invitationDtos) {
+        return InvitationResponseDTO.getInvitations.builder()
+                .invitations(invitationDtos)
+                .build();
+    }
+}
+
