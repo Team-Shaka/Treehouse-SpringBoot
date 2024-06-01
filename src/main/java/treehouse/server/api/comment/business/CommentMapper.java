d1b89d7f847ae3b0711d4c11115be8275c7e1e73
HyoBN <qls0786@naver.com>
2024-06-01 16:52:17 +0900

    ✨ Feat: 댓글 목록 조회 API 및 페이징 구현

--- src/main/java/treehouse/server/api/comment/business/CommentMapper.java
+++ src/main/java/treehouse/server/api/comment/business/CommentMapper.java
@@ -1,4 +1,36 @@
 package treehouse.server.api.comment.business;
 
+import lombok.AccessLevel;
+import lombok.NoArgsConstructor;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+import treehouse.server.api.comment.presentation.dto.CommentResponseDTO;
+import treehouse.server.api.member.business.MemberMapper;
+import treehouse.server.global.entity.comment.Comment;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
 public class CommentMapper {
+
+
+    public static CommentResponseDTO.CommentInfoDto toCommentInfoDto(Comment comment) {
+        return CommentResponseDTO.CommentInfoDto.builder()
+                .commentedAt(comment.getCreatedAt().toString())
+                .commentId(comment.getId())
+                .context(comment.getContent())
+//                .reactionList(comment.) 리액션 관련 로직 구현 후 추가.
+                .memberProfile(MemberMapper.toGetMemberProfile(comment.getWriter()))
+                .build();
+
+    }
+
+    public static CommentResponseDTO.CommentListDto toCommentListDto(List<Comment> commentList) {
+        List<CommentResponseDTO.CommentInfoDto> commentInfoDtoList = commentList.stream()
+                .map(comment ->
+                        toCommentInfoDto(comment)).toList();
+        return CommentResponseDTO.CommentListDto.builder()
+                .commentList(commentInfoDtoList)
+                .build();
+    }
 }
--- src/main/java/treehouse/server/api/comment/presentation/dto/CommentResponseDTO.java
+++ src/main/java/treehouse/server/api/comment/presentation/dto/CommentResponseDTO.java
@@ -1,4 +1,36 @@
 package treehouse.server.api.comment.presentation.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import treehouse.server.api.member.presentation.dto.MemberResponseDTO;
+import treehouse.server.api.reaction.presentation.dto.ReactionResponseDTO;
+
+import java.util.List;
+
 public class CommentResponseDTO {
+
+
+    @Builder
+    @Getter
+    @NoArgsConstructor
+    @AllArgsConstructor
+    public static class CommentInfoDto{
+
+        MemberResponseDTO.getMemberProfile memberProfile;
+        ReactionResponseDTO.reactionSimpleResponseDTO reactionList;
+        Long commentId;
+        String context;
+        String commentedAt;
+    }
+
+    @Builder
+    @Getter
+    @NoArgsConstructor
+    @AllArgsConstructor
+    public static class CommentListDto{
+        List<CommentInfoDto> commentList;
+
+    }
 }
