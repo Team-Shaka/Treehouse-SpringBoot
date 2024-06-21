f60fd726ebeb3507e951c0c30e2d095dc52136d4
CYY1007 <younguk86@gmail.com>
2024-06-21 14:15:10 +0900

    :sparkles: Feat : 초대하기 API 구현

--- src/main/java/treehouse/server/api/invitation/business/InvitationMapper.java
+++ src/main/java/treehouse/server/api/invitation/business/InvitationMapper.java
@@ -5,10 +5,15 @@ import lombok.NoArgsConstructor;
 import org.springframework.stereotype.Component;
 import treehouse.server.api.invitation.presentation.dto.InvitationResponseDTO;
 import treehouse.server.global.entity.Invitation.Invitation;
+import treehouse.server.global.entity.Invitation.InvitationStatus;
 import treehouse.server.global.entity.User.User;
 import treehouse.server.global.entity.User.UserRole;
 import treehouse.server.global.entity.User.UserStatus;
+import treehouse.server.global.entity.member.Member;
+import treehouse.server.global.entity.treeHouse.TreeHouse;
 
+import java.time.LocalDateTime;
+import java.time.temporal.ChronoUnit;
 import java.util.List;
 
 @Component
@@ -41,6 +46,29 @@ public class InvitationMapper {
     public InvitationResponseDTO.invitationAccept toInvitationResult(Long treeHouseId){
         return InvitationResponseDTO.invitationAccept.builder()
                 .treehouseId(treeHouseId)
+                .build();
+    }
+
+    public Invitation toInvitation(String phoneNumber, Member sender, User receiver, TreeHouse treeHouse){
+
+
+        LocalDateTime now = LocalDateTime.now();
+
+        // 7일 뒤 시간 계산하기
+        LocalDateTime sevenDaysLater = now.plus(7, ChronoUnit.DAYS);
+
+        return Invitation.builder()
+                .sender(sender)
+                .receiver(receiver)
+                .phone(phoneNumber)
+                .expiredAt(sevenDaysLater)
+                .status(InvitationStatus.PENDING)
+                .build();
+    }
+
+    public InvitationResponseDTO.createInvitation toCreateInvitationDTO (Invitation invitation){
+        return InvitationResponseDTO.createInvitation.builder()
+                .invitationId(invitation.getId())
                 .build();
     }
 }
