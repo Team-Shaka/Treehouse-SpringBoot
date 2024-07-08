04d85c173ab2b8b05c84064888fd0ff0102ea0b6
구환준/모건 <koojun99@naver.com>
2024-07-08 09:40:32 +0900

    ✨ Feat: Treehouse 생성

---
+++ src/main/java/treehouse/server/api/treehouse/business/TreehouseService.java
@@ -0,0 +1,32 @@
+package treehouse.server.api.treehouse.business;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import treehouse.server.api.member.implementation.MemberQueryAdapter;
+import treehouse.server.api.treehouse.implementation.TreehouseCommandAdapter;
+import treehouse.server.api.treehouse.presentation.dto.TreehouseRequestDTO;
+import treehouse.server.api.treehouse.presentation.dto.TreehouseResponseDTO;
+import treehouse.server.global.entity.treeHouse.TreeHouse;
+
+@Service
+@RequiredArgsConstructor
+@Slf4j
+@Transactional
+public class TreehouseService {
+
+    private final TreehouseCommandAdapter treehouseCommandAdapter;
+
+    private final MemberQueryAdapter memberQueryAdapter;
+
+    public TreehouseResponseDTO.createTreehouse createTreehouse(TreehouseRequestDTO.createTreehouse request) {
+
+        TreeHouse treehouse = TreehouseMapper.toTreehouse(request);
+        TreeHouse savedTreehouse = treehouseCommandAdapter.saveTreehouse(treehouse);
+
+        return TreehouseMapper.toCreateTreehouse(savedTreehouse);
+    }
+
+
+}
