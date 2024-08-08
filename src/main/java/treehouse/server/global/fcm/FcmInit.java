edd2975f490ec5a7235e7854b5e88e6219927e4e
HyoBN <qls0786@naver.com>
2024-08-08 15:51:04 +0900

    ✨ Feat: fcm init 세팅

---
+++ src/main/java/treehouse/server/global/fcm/FcmInit.java
@@ -0,0 +1,33 @@
+package treehouse.server.global.fcm;
+
+import com.google.auth.oauth2.GoogleCredentials;
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.FirebaseOptions;
+import jakarta.annotation.PostConstruct;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.ClassPathResource;
+
+import java.io.IOException;
+import java.io.InputStream;
+
+@Configuration
+public class FcmInit {
+    @Value("${firebase.admin-sdk}")
+    private String FIREBASE_KEY_PATH;
+
+    @PostConstruct
+    public void firebaseMessaging() throws IOException {
+        ClassPathResource resource = new ClassPathResource(FIREBASE_KEY_PATH);
+        InputStream credentialStream = resource.getInputStream();
+        try {
+            FirebaseOptions options = FirebaseOptions.builder()
+                    .setCredentials(GoogleCredentials.fromStream(credentialStream))
+                    .build();
+            FirebaseApp.initializeApp(options);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+}
