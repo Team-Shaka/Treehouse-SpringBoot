388bbf52a7a4dc64fb0ac7639fbb5d93ac45fd4d
HyoBN <qls0786@naver.com>
2024-08-08 17:43:00 +0900

    ✨ Feat: Feign 기반 FCM 로직 구현

---
+++ src/main/java/treehouse/server/global/feign/config/FCMFeignConfig.java
@@ -0,0 +1,27 @@
+package treehouse.server.global.feign.config;
+
+import feign.Logger;
+import feign.RequestInterceptor;
+import feign.codec.ErrorDecoder;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import treehouse.server.global.feign.exception.FeignExceptionErrorDecoder;
+
+@RequiredArgsConstructor
+public class FCMFeignConfig {
+
+    @Bean
+    public RequestInterceptor requestInterceptor(){
+        return template -> template.header("Content-Type", "application/json;charset=UTF-8");
+    }
+
+    @Bean
+    public ErrorDecoder errorDecoder() {
+        return  new FeignExceptionErrorDecoder();
+    }
+
+    @Bean
+    Logger.Level feignLoggerLevel() {
+        return Logger.Level.FULL;
+    }
+}
